7bdb993a729b57a63a846ca788aee2e88a1322ec
NikhilUprety <105978476+NikhilUprety@users.noreply.github.com>
2023-01-12 20:51:34 +0545

    Add files via upload

---
+++ eventuse.js
@@ -0,0 +1,11 @@
+function increment(){
+    let numb=document.getElementById("number").innerHTML
+numb++;
+document.getElementById("number").innerHTML=numb
+}
+function decrement(){
+    let value=document.getElementById("number").innerHTM
+    numb--;
+    console.log(value)
+    document.getElementById("number").innerHTML=numb;
+}
